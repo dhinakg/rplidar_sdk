d8427515e7a1a0f48af9bfd2093ee12b2bfc6c25
Dhinak G <dhinak2004@gmail.com>
2019-03-06 16:28:52 -0500

    Update main.cpp

--- sdk/app/ultra_simple/main.cpp
+++ sdk/app/ultra_simple/main.cpp
@@ -207,11 +207,11 @@ int main(int argc, const char * argv[]) {
         if (IS_OK(op_result)) {
             drv->ascendScanData(nodes, count);
             for (int pos = 0; pos < (int)count ; ++pos) {
-                printf("%s theta: %03.2f Dist: %08.2f Q: %d \n", 
-                    (nodes[pos].sync_quality & RPLIDAR_RESP_MEASUREMENT_SYNCBIT) ?"S ":"  ", 
+                printf("%llu,%03.2f,%08.2f%s\n",
+                    nodes[pos].timestamp,
                     (nodes[pos].angle_q6_checkbit >> RPLIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f,
                     nodes[pos].distance_q2/4.0f,
-                    nodes[pos].sync_quality >> RPLIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
+                    (nodes[pos].sync_quality & RPLIDAR_RESP_MEASUREMENT_SYNCBIT) ?"s":"");
             }
         }
 
